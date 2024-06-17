2194401b250fdcff61cb5a6346968b31813fb342
Antonio <devantonio.fer@gmail.com>
2024-06-17 10:04:29 -0300

    chore: adding debug-toolbar

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from .settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls'))
 ]
+
+if DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
