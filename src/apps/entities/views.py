32bd521d3fcba1c3e08c3def5eb0cee6ac2de558
Antônio Fernandes <devantonio.fer@gmail.com>
2024-07-11 21:46:49 -0300

    feat: tela de certificados

--- src/apps/entities/views.py
+++ src/apps/entities/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+from apps.courses.models import Certificate
+
 
 def user_profile(request):
-  if request.method == "GET":
-    return render(request, "pages/user_profile.html")
+    if request.method == "GET":
+        return render(request, "pages/profile/user_profile.html")
+
+
+@require_http_methods(["GET"])
+def user_certificates(request):
+    certificates = Certificate.objects.filter(
+        student_object_id=request.user.student.id,
+        student_content_type=request.user.student.get_content_type()
+    )
+    return render(request, "pages/profile/certificates.html", {
+        "certificates": certificates
+    })
